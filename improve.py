537debd923b0aeee31c305bfd300d7fd533de516
cesar <cesar@nyogtha.(none)>
2010-10-25 00:34:44 -0200

    Added the improve.py script. This script apply some transformations in the Correios images in order to make it easier for the tesseract to process them.

---
+++ improve.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python
+# aplicar threshold 175
+# aumentar o tamanho em 300% usando cubic
+# conveter para indexado (PB)
+import sys
+import getopt
+from PIL import Image
+
+def improve(image):
+
+    try:
+        im = Image.open(image)
+    except IOError:
+        print "Error: Unable to open '{0}'".format(image)
+        return None
+
+    # Convert the image to grayscale
+    im = im.convert('L')
+
+    # Apply a 175 threshold
+    im = im.point(lambda i: i if i < 175 else 255)
+
+    # Scale it by 300%
+    (width, height) = im.size
+    im = im.resize((width*3, height*3), Image.BICUBIC)
+
+    return im
+
+if __name__ == "__main__":
+    if len(sys.argv) == 3:
+        image = sys.argv[1]
+        output = sys.argv[2]
+
+        im = improve(image)
+        im.save(output)
+    else:
+        print "improve.py <filename> <output>"
